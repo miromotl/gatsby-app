175da6e6291310ab6b0eefbb7b09ae785365f166
Miro Motl <miromotl@yahoo.com>
2020-10-02 12:37:41 +0200

    added layout to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-export default ({ data }) => (
-    <React.Fragment>
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const aboutPage = ({ data }) => (
+    <Layout>
+        <SEO title="about page" />
         <h2>{data.site.siteMetadata.title}</h2>
         <div>{data.site.siteMetadata.description}</div>
         <Link to="/">Back to home</Link>
-    </React.Fragment>
+    </Layout>
 )
+
+export default aboutPage
 
 export const query = graphql ` 
     query {
